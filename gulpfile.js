78f70bdae94d1b260e98e877bb16d92296c64002
Andreas Hubel <andreas.hubel@br.de>
2021-08-27 21:25:13 +0200

    upgrade gulp to v4 to be node 12 compatible

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
+    sass = require('gulp-sass')(require('sass')),
     coffee = require('gulp-coffee'),
     watch = require('gulp-watch'),
     uglify = require('gulp-uglify'),
@@ -18,14 +18,14 @@ var paths = {
   stylesheets: ['./src/stylesheets/*.scss'],
   main_javascript: ['./src/coffee/app.coffee'],
   javascripts: ['./src/coffee/*.coffee'],
-  html: ['./src/html/button.html', './src/html/popup.html'],
+  html: ['./src/html/button.html'],
   images: ['./src/images/**'],
   fonts: ['./src/fonts/**']
 };
 
 gulp.task('stylesheets', function() {
   return gulp.src(paths.stylesheets)
-  .pipe(sourcemaps.init())
+    .pipe(sourcemaps.init())
     .pipe(sass({style: 'compressed'}))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./dist/stylesheets'))
@@ -34,7 +34,7 @@ gulp.task('stylesheets', function() {
     .pipe(connect.reload())
 })
 
-gulp.task('javascripts', function() {
+gulp.task('javascripts', function(done) {
   gulp.src(paths.main_javascript, {read: false})
     .pipe(browserify({
       transform: ['coffeeify'],
@@ -46,47 +46,60 @@ gulp.task('javascripts', function() {
     .pipe(gzip())
     .pipe(gulp.dest('./dist/javascripts'))
     .pipe(connect.reload())
+
+   done()
 })
 
-gulp.task('html', function() {
+gulp.task('html', function(done) {
   gulp.src(paths.html)
     .pipe(gulp.dest('./dist'))
     .pipe(connect.reload())
+
+  done()
 })
 
-gulp.task('images', function() {
+gulp.task('images', function(done) {
   gulp.src(paths.images)
     .pipe(gulp.dest('./dist/images'))
     .pipe(connect.reload())
+
+  done()
 })
 
-gulp.task('fonts', function() {
+gulp.task('fonts', function(done) {
   gulp.src(paths.fonts)
     .pipe(gulp.dest('./dist/fonts'))
     .pipe(connect.reload())
+
+  done()
 })
 
-gulp.task('default', ['stylesheets', 'javascripts', 'html', 'images', 'fonts'])
+gulp.task('default', gulp.series('stylesheets', 'javascripts', 'html', 'images', 'fonts'))
 
-gulp.task('watch', function() {
-  // Watch .scss files
-  gulp.watch(paths.stylesheets, ['stylesheets'])
-  // Watch .js files
-  gulp.watch(paths.javascripts, ['javascripts'])
-  // Watch .html files
-  gulp.watch(paths.html, ['html'])
-  // Watch images files
-  gulp.watch(paths.images, ['images'])
-  // Watch font files
-  gulp.watch(paths.fonts, ['fonts'])
+gulp.task('watch', function(done) {
+  gulp.parallel(
+    // Watch .scss files
+    () => gulp.watch(paths.stylesheets, ['stylesheets']),
+    // Watch .js files
+    () => gulp.watch(paths.javascripts, ['javascripts']),
+    // Watch .html files
+    () => gulp.watch(paths.html, ['html']),
+    // Watch images files
+    () => gulp.watch(paths.images, ['images']),
+    // Watch font files
+    () => gulp.watch(paths.fonts, ['fonts']),
+  );
+  done()
 })
 
-gulp.task('connect', function() {
+gulp.task('connect', function(done) {
   connect.server({
     root:'./',
     livereload: true
   });
+
+  done();
 });
 
 // Serve
-gulp.task('serve', ['default', 'connect', 'watch']);
+gulp.task('serve', gulp.series('default', 'connect', 'watch'));
